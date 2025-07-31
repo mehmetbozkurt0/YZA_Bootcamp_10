98527c5f2aed94c38241ae7a6f2b2dc9d961f118
Mehmet Bozkurt <mehmet.64.b@gmail.com>
2025-07-31 16:47:07 +0300

    kayıt ekranı eklendi;günün sözü,  mood ve feedback kısımları sinamik hale getirildi

--- app/crud.py
+++ app/crud.py
@@ -1,3 +1,4 @@
+from datetime import date
 from app.database import cursor, db
 
 def get_all():
@@ -49,3 +50,59 @@ def update_task_by_id(user_id, task_id, completed):
         db.commit()
     except Exception as e:
         print("Güncelleme hatası: ",e)
+
+def register_user(email,password,username):
+    try:
+        cursor.execute("INSERT INTO tblUser (Email, Password, UserName) VALUES (?,?,?)",(email,password,username))
+        db.commit()
+    except Exception as e:
+        print("Kullanıcı kaydedilemedi: ",e)
+
+def check_user_exist(email):
+    cursor.execute("SELECT * FROM tblUser WHERE Email = ?", (email,))
+    return cursor.fetchone() is not None
+
+def get_daily_quote():
+    try:
+        cursor.execute("SELECT COUNT(*) FROM tblQuote")
+        count = cursor.fetchone()[0]
+        if count == 0:
+            return None
+
+        today = date.today()
+        day_key = int(today.strftime("%Y%m%d"))
+        quote_id = (day_key % count) + 1
+
+        cursor.execute("SELECT Text FROM tblQuote WHERE Id = ?", (quote_id,))
+        row = cursor.fetchone()
+        return row[0] if row else None
+    except Exception as e:
+        print("Günün sözü yüklenemedi:", e)
+        return None
+
+
+def get_reflection(user_id):
+    today = date.today()
+    cursor.execute("SELECT Mood, Feedback FROM tblReflection WHERE UserId = ? AND ReflectionsDate = ?", (user_id, today))
+    return cursor.fetchone()
+
+def save_or_update_reflection(user_id, mood, feedback):
+    today = date.today()
+    cursor.execute("SELECT Id FROM tblReflection WHERE UserId = ? AND ReflectionsDate = ?", (user_id, today))
+    row = cursor.fetchone()
+
+    if row:
+        cursor.execute("UPDATE tblReflection SET Mood = ?, Feedback = ? WHERE Id = ?", (mood, feedback, row[0]))
+    else:
+        cursor.execute("INSERT INTO tblReflection (UserId, Mood, Feedback) VALUES (?, ?, ?)", (user_id, mood, feedback))
+    db.commit()
+
+
+
+
+
+
+
+
+
+
